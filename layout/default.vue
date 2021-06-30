98ea2d0bfd16886f7aed0b81f57b649406b142fe
Rafael Oliveira <rafael.tragueta79@gmail.com>
2021-06-30 20:23:41 -0300

    initial setup

---
+++ layout/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <!-- <PortfolioHeader /> -->
+    <Nuxt />
+    <!-- <PortfolioFooter /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    // PortfolioHeader,
+    // PortfolioFooter
+  }
+}
+</script>
+
+<style>
+/* font-family: 'Open Sans', sans-serif; */
+
+:root {
+  --dark: rgb(20, 20, 20);
+  --white: rgb(255, 255, 255);
+  --highlight: rgb(255, 181, 36);
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1em;
+  line-height: 1.6;
+  background: var(--dark);
+  color: var(--white);
+}
+
+a {
+  color: var(--highlight);
+  text-decoration: none;
+}
+</style>
