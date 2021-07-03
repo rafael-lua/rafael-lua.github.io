191fb646483568fa42c498c2f5812f89ac33e3f5
Rafael Oliveira <rafael.tragueta79@gmail.com>
2021-07-03 10:26:27 -0300

    create footer component

---
+++ components/layout/PortfolioFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="mt-16 flex justify-center py-5 px-16 bg-color"></footer>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioFooter',
+}
+</script>
+
+<style scoped>
+.bg-color {
+  background-color: rgb(var(--highlight));
+}
+</style>
