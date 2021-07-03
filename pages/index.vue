29f72391a89a52b48fc1fb744f5b6d6dbeafa099
Rafael Oliveira <rafael.tragueta79@gmail.com>
2021-07-03 12:10:21 -0300

    update index with content section

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <IntroductionSection />
     <SectionDivider />
+    <ContentSection />
   </div>
 </template>
 
 <script>
 import IntroductionSection from '@/components/sections/IntroductionSection.vue'
 import SectionDivider from '@/components/sections/SectionDivider.vue'
+import ContentSection from '@/components/sections/ContentSection.vue'
 
 export default {
   components: {
     IntroductionSection,
     SectionDivider,
+    ContentSection,
   },
 }
 </script>
