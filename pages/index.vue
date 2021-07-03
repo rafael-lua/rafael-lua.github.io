896e29b076d2f07b28df2ee074b1ecc9b3aca179
Rafael Oliveira <rafael.tragueta79@gmail.com>
2021-07-03 10:22:52 -0300

    update index to use section divider

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <IntroductionSection />
+  <div>
+    <IntroductionSection />
+    <SectionDivider />
+  </div>
 </template>
 
 <script>
 import IntroductionSection from '@/components/sections/IntroductionSection.vue'
+import SectionDivider from '@/components/sections/SectionDivider.vue'
 
 export default {
   components: {
     IntroductionSection,
+    SectionDivider,
   },
 }
 </script>
